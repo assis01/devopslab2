455bcfd018a896fe1f360d582f6fcbd762e4d98a
Leonardo Alves de Assis <aa.ssis@yahoo.com.br>
2021-08-16 16:34:26 -0300

    Bug Removido

--- app.py
+++ app.py
@@ -9,14 +9,5 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "Laboratório Pipeline DevOps -> Grupo 8"
 
-@app.route('/bug')   
-def bad():
-    try:
-        raise TypeError()
-    except TypeError as e:
-        print(e)
-    except TypeError as e:
-        print("Duplicado, ou seja, nunca vai entrar aqui.")
-
 if __name__ == '__main__':
    app.run(debug=True)
