499b2653222ced05085cf2701d91472327828a51
Leonardo Alves de Assis <aa.ssis@yahoo.com.br>
2021-08-16 20:50:02 -0300

    Coverage Ativado

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Pipeline DevOps -> Grupo 8"
+    return "Pipeline-DevOps"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,12 +2,14 @@ from app import app
 import unittest
 
 class Test(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-    def test_requisicao(self):
-        self.assertEqual(self.result.status_code, 200)
-    def test_conteudo(self):
-        self.assertEqual(self.result.data.decode('utf-8'), "Pipeline DevOps -> Grupo 8")
+   def setUp(self):
+       self.app = app.test_client()
+
+   def test_requisicao(self):
+       self.assertEqual(self.result.status_code, 200)
+
+   def test_conteudo(self):
+       self.assertEqual(self.result.data.decode('utf-8'), "Pipeline-DevOps")
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
