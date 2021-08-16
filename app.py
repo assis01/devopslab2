d7513d6016d340c742a3cfd3c366b0c4ada7b2f2
Leonardo Alves de Assis <aa.ssis@yahoo.com.br>
2021-08-16 14:18:37 -0300

    alteração de texto

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Laboratório Pipeline DevOps -> Grupo 8"
 
 if __name__ == '__main__':
    app.run(debug=True)
