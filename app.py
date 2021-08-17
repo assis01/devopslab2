2f206cdf62a58a67399bed16d81c532eaf2e4b35
Leonardo Alves de Assis <aa.ssis@yahoo.com.br>
2021-08-16 21:53:34 -0300

    Coverage Ativado

--- app.py
+++ app.py
@@ -5,5 +5,14 @@ csrf = CSRFProtect(app)
 @app.route("/")
 def pagina_inicial():
     return "Pipeline-DevOps"
+@app.route('/bug')
+def bad():  
+    try:
+        raise TypeError()
+    except TypeError as e:
+        print(e)
+    except TypeError as e:
+        print("Duplicado, ou seja, nunca vai entrar aqui.")
+
 if __name__ == '__main__':
     app.run(debug=True)
