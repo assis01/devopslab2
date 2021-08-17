7332718b98a72cc00dc64bc5581a483d9bd1c2b1
Leonardo Alves de Assis <aa.ssis@yahoo.com.br>
2021-08-16 21:09:40 -0300

    Coverage Ativado

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
-
-
 app = Flask(__name__)
-
 csrf = CSRFProtect(app)
-
 @app.route("/")
 def pagina_inicial():
     return "Pipeline-DevOps"
-
 if __name__ == '__main__':
     app.run(debug=True)
